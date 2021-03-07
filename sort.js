691959b68ff1d8f6c3bbbc53905af3bb968d4f84
Edson <edsoncondorimm@gmail.com>
2021-03-07 11:40:28 -0300

    first commit

---
+++ sort.js
@@ -0,0 +1,12 @@
+/*
+let ordenarNombre = function()
+{
+    let claves = Object.keys(datos).sort()
+    claves.forEach(e => {
+        console.log(e)
+        console.log("ordenadas :s")
+       pegarHTML('#divisasOrdenadas',`${datos[e]}`) 
+    });
+}
+
+*/
